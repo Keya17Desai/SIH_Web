64db42a9ad98f4e51e39ff273ea6f51afc72e36c
Anurag Gour <gour3551@gmail.com>
2023-10-06 21:10:41 +0530

    Basic Structure Created

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+import "../assets/style/notfound.css";
+const NotFound = () => {
+  return <Layout>NotFound</Layout>;
+};
+
+export default NotFound;
