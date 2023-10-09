a5cc065db12536695ef3e34a1938d4bfc93d1bae
KeyaDesai <121417626+Keya17Desai@users.noreply.github.com>
2023-10-09 21:31:54 +0530

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import "../../assets/style/footer.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-content">
+          <div className="copyright">
+            &copy; 2023 YaanDarshak
+          </div>
+          <div className="social-icons">
+            <a href="#" className="social-icon">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a href="#" className="social-icon">
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a href="#" className="social-icon">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
