9d6999f0303e9a96ee2178f55e4b75499423dfc2
KeyaDesai <121417626+Keya17Desai@users.noreply.github.com>
2023-10-09 21:37:48 +0530

    Update Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import "../../assets/style/header.css";
+import { Link } from 'react-router-dom'; // If you're using React Router for navigation
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header-content">
+          <h1 className="logo">
+            <Link to="/">Yaandarshak</Link>
+          </h1>
+          <nav className="nav">
+            <ul>
+              <li>
+                <Link to="./pages/Home">Home</Link>
+              </li>
+              <li>
+                <Link to="./pages/Schedule">Schedule</Link>
+              </li>
+              <li>
+                <Link to="./pages/Track">Track</Link>
+              </li>
+              <li>
+                <Link to="./pages/Contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
